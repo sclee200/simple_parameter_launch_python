b02d8f4c1ee667c54b9d53293c633b45756df301
sclee200 <chulslee20@gmail.com>
2022-01-13 12:15:11 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob #위에 2개 추가
+
+package_name = 'simple_parameter_launch_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py')),#추가
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lsc',
+    maintainer_email='chulslee20@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "basic_parameter = simple_parameter_launch_python.basic_parameter:main"
+        ],
+    },
+)
